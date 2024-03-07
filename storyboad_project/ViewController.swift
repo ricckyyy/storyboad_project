b4c08ff03649552aa087a267c2fc53a6973d33de
R-T <csri16001@gmail.com>
2024-03-07 23:18:53 +0900

    Initial Commit

---
+++ storyboad_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyboad_project
+//
+//  Created by Riki T on 2024/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
